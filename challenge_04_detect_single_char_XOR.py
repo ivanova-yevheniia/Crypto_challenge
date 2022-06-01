e81fd45106a9f48b47a075d93e45d09827a88bc8
Asus <ivanova.yevheniia@gmail.com>
2022-06-01 15:55:00 +0300

    testing challenge 4

--- challenge_04_detect_single_char_XOR.py
+++ challenge_04_detect_single_char_XOR.py
@@ -1,13 +1,19 @@
 from challenge_03_single_byte_XOR import crack_xor_cipher, ScoredGuess
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     with open("text_challenge_4.txt") as f:
         lines = [bytes.fromhex(line.strip()) for line in f]
 
     overall_best = ScoredGuess()
     for line in lines:
+        print(end=".", flush=True)
         candidate = crack_xor_cipher(line)
         overall_best = min(overall_best, candidate)
 
+    if overall_best.ciphertext is None:
+       exit("no ciphertext found")
 
-    print(overall_best)
+    print()
+    print(f"{lines.index(overall_best.ciphertext)=}")
+    print(f"{overall_best.key=}")
+    print(f"{overall_best.plaintext=}")
