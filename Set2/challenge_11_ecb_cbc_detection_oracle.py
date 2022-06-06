d242f92fff0377003c05d9fe45ff7d71c564fa69
Asus <ivanova.yevheniia@gmail.com>
2022-06-06 19:32:18 +0300

    challenge 11 completed

--- Set2/challenge_11_ecb_cbc_detection_oracle.py
+++ Set2/challenge_11_ecb_cbc_detection_oracle.py
@@ -1,5 +1,4 @@
 from Set2.challenge_09_padding import pad
-from Set1.challenge_08_detect_AES_in_ECB_mode import bytes_to_chunks
 from challenge_10_cbc_mode import AES
 from random import randint
 from os import urandom
@@ -29,21 +28,16 @@ def ECB_CBC_detect(oracle: type[encryption_oracle_interface]) -> str:
     data = b'A' * 48
     ciphertext = oracle.encryption(data)
 
-    return 'ECB' if contains_repeated_blocks(ciphertext) else 'CBC'
+    blocks = [ciphertext[i:i+AES.block_size] for i in range(0, len(ciphertext), AES.block_size)]
 
-def contains_repeated_blocks(ciphertexts: bytes) -> bool:
-    for i, ciphertext in enumerate(ciphertexts):
-        num_blocks = len(ciphertext) // 16
-        num_uniq_blocks = len(set(bytes_to_chunks(ciphertext, 16)))
-        repeated_blocks = num_blocks - num_uniq_blocks
-        return repeated_blocks != 0
+    return 'ECB' if len(blocks) != len(set(blocks)) else 'CBC'
 
 if __name__ == '__main__':
     oracle = encryption_oracle()
     detections = []
 
     for _ in range(10):
-        detections.append(ECB_CBC_detect(encryption_oracle))
+        detections.append(ECB_CBC_detect(oracle))
 
-    print(f"Correct: {detections == oracle.arr}")
+    print(f"Correct: {detections == oracle.arr} \nactual: {oracle.arr} \nexpect: {detections}")
 
